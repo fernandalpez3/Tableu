dd4e460be7daabfbee728d2bed8d76439695388a
Eduardo Merino <A01630088@itesm.mx>
2018-03-21 21:06:21 -0600

    incomplete id on-click

--- ReservationsVue 6.28.09 PM/src/components/Hello.vue
+++ ReservationsVue 6.28.09 PM/src/components/Hello.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-  document.getElementById('log_out').innerHTML += "Log Out";
+document.getElementById('log_out').innerHTML += "Log Out";
 </script>
 
 <script>
@@ -36,12 +36,19 @@ export default {
         this.$router.replace('login')
       })
     },
+    click_restaurant: function(event) {
+      if (event) {
+        targetId = event.currentTarget.id;
+        console.log(targetId);
+        alert(targetId);
+      }
+    },
     show_cards: function(){
-      
+
       var rootRef = firebase.database().ref();
       rootRef.once("value").then(function(snapshot) {
         snapshot.child("restaurants").forEach(function(val){
-          $('#restaurant_list').append('<div><div class="col-lg-3 col-md-3 col-xs-6 card"><a href="#" class="d-block mb-4 h-100" id="'+ val.val() +'"><h4>' + val.child("name").val() + '</h4></a></div></div>');
+          $('#restaurant_list').append('<div><div class="col-lg-3 col-md-3 col-xs-6 card"><a href="#" class="d-block mb-4 h-100" id="'+ val.key +'" v-on:click.native="click_restaurant"><h4>' + val.child("name").val() + '</h4></a></div></div>');
           //<div id="restaurant_list"><div class="col-lg-3 col-md-3 col-xs-6 card"><a href="#" class="d-block mb-4 h-100"><img class="img-fluid img-thumbnail" src="' + val.child("image").val() + '" alt=""><h4>' + val.child("name").val() + '</h4></a> </div></div>
         });
       });
